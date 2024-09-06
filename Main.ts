ed54c99d80e0ffff1940bb41dc755eb496c2113c
Alveena-Ali <163353918+Alveena-Ali@users.noreply.github.com>
2024-09-07 00:10:29 +0500

    Que 10: Adding Comments

---
+++ Main.ts
@@ -0,0 +1,18 @@
+// Que 10: Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t
+// have anything specific to write because your programs are too simple at this point, just add your name and the current
+// date at the top of each program file. Then write one sentence describing what the program does.
+
+let a: number = 10;
+let b: number = 20;
+
+// Addition
+console.log(`${a} + ${b} = ${a + b}`);
+
+// Subtraction
+console.log(`${b} - ${a} = ${b - a}`);
+
+// Multiplication
+console.log(`${a} * ${b} = ${a * b}`);
+
+// Division
+console.log(`${b} / ${a} = ${b / a}`);
